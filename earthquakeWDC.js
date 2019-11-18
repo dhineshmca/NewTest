5d7a8e23946bc1ef6f3bc3ad412f31fcbbeb5a8f
Dhineshshankar <dhineshshankar@gmail.com>
2019-11-18 14:20:04 +0530

    Update earthquakeWDC.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -2,6 +2,30 @@
     var myConnector = tableau.makeConnector();
 
    myConnector.getSchema = function (schemaCallback) {
+    var cols = [{
+        id: "id",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "mag",
+        alias: "magnitude",
+        dataType: tableau.dataTypeEnum.float
+    }, {
+        id: "title",
+        alias: "title",
+        dataType: tableau.dataTypeEnum.string
+    }, {
+        id: "location",
+        dataType: tableau.dataTypeEnum.geometry
+    }];
+
+    var tableSchema = {
+        id: "earthquakeFeed",
+        alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+        columns: cols
+    };
+
+    schemaCallback([tableSchema]);
+}; {
     tableau.log("Hello WDC!");
    }; 
 
