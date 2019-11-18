a3d186cf4e463b00faac1274a3e7541b2f512335
Dhineshshankar <dhineshshankar@gmail.com>
2019-11-18 14:12:29 +0530

    Update earthquakeWDC.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -9,11 +9,7 @@
 
     };
 
-    tableau.registerConnector(myConnector);
-    $(document).ready(function () {
-    $("#submitButton").click(function () {
-        tableau.connectionName = "USGS Earthquake Feed";
-        tableau.submit();
+   
 
    
     });
