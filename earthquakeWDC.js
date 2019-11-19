0190acdda2bdad3d0222102f6d33279ebc9f6a73
Dhineshshankar <dhineshshankar@gmail.com>
2019-11-19 13:01:59 +0530

    Update earthquakeWDC.js

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -55,10 +55,9 @@
   tableau.registerConnector(myConnector);
   // Create event listeners for when the user submits the form
   $(document).ready(function() {
-        $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-            tableau.submit(); // This sends the connector object to Tableau
-        });
-
+      $("#submitButton").click(function() {
+          tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+          tableau.submit(); // This sends the connector object to Tableau
+      });
 });
 })();
